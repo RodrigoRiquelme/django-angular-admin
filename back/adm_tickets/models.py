5c73fc4fdd1f57e65ca325241b8e1c11cae1aed4
Rodrigo Riquelme <rodrigo.riquelme@2brains.cl>
2019-05-29 10:09:19 -0400

    removed compiled files

--- back/adm_tickets/models.py
+++ back/adm_tickets/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from aenum import Enum
 
 
-class TicketStatus(Enum):
-    OPEN = "Abierto"
-    PENDING = "Pendiente"
-    IN_PROCESS = "En Proceso"
-    CLOSED = "Resuelto y cerrado"
-
+TICKET_STATUS = (
+    ('OPEN', 'Draft'),
+    ('PENDING', 'Published'),
+    ('IN_PROCESS', 'En Proceso'),
+    ('CLOSED', 'Resuelto y cerrado')
+)
 
 
 class Ticket(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=10, choices=TICKET_STATUS)
     created_at = models.DateTimeField(auto_now=True)
